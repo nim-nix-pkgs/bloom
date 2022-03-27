e8b9968d2aa7b792f344f0bba105bd90b5df1f79
hugosenari <hugosenari@gmail.com>
2022-03-27 17:58:49 -0300

    chore: re index bloom

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-bloom-master.ref   = "refs/heads/master";
   inputs.src-bloom-master.owner = "boydgreenfield";
   inputs.src-bloom-master.repo  = "nimrod-bloom";
+  inputs.src-bloom-master.dir   = "";
   inputs.src-bloom-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
